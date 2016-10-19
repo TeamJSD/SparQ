5c726b3e8f08ae54801168f1b093fe612b48cd85
jaycetan <jaycetan@gmail.com>
2016-10-18 19:59:53 -0700

    updated transpiler to import the correct dependency according to the userID

---
+++ devUserDbs/123abc_db.js
@@ -0,0 +1,41 @@
+import Sequelize from 'sequelize';
+import _ from 'lodash';
+
+const Conn = new Sequelize(
+  'DBNAME123ABC', //name of users individual db.
+  '123abc', // username
+  '123abc', { //password
+    dialect: 'postgres', //always postgres
+    host: 'localhost' //instance
+  }
+);
+
+const Person = Conn.define('person', {
+email: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+age: {
+      type: Sequelize.INTEGER,
+      allowNull: true
+    },
+});
+
+const Post = Conn.define('post', {
+Title: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+Content: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+});
+
+Person.hasMany(Post);
+Post.belongsTo(Person);
+export default Conn;
