08381eda76cdd2065a538778e135e1603eef7de2
Derrick Ung <derrickrung@yahoo.com>
2016-11-01 11:59:13 -0700

    ready to present

--- server/db/sparqDb.js
+++ server/db/sparqDb.js
@@ -8,8 +8,8 @@ let sampleSchemaModel = JSON.stringify(testFixture);
 
 const Conn = new Sequelize(
   process.env.NODE_DBNAME,
-  null, //replace later with process.env.NODE_DBUSERNAME,,
-  null, //replace later with process.env.NODE_DBPASSWORD,
+  null, //replace later with process.env.NODE_DBUSERNAME,
+  null, //replace later with process.env.NODE_DBPASSWORD
   , {
     dialect: 'postgres', //always postgres
     host: process.env.NODE_DBHOST
